bcc375ed3820c825fc010ae7411516b1db5669ee
Mahdi Raza Khunt <82615934+mrkhunt@users.noreply.github.com>
2024-12-16 01:22:45 -0500

    feat: day7p2

---
+++ 2024/day7p2.py
@@ -0,0 +1,24 @@
+data = open("day7p2_input.txt").read().split("\n")
+
+sum = 0
+
+for equation in data:
+    equation = equation.split(":")
+    testValue = int(equation[0])
+    testNumbers = [int(i) for i in equation[1].split()]
+    def evalSum(testValue, testNumbers):
+        if len(testNumbers) == 1:
+            if testValue == testNumbers[0]:
+                return True
+            else:
+                return False
+        else:
+            if evalSum(testValue, [testNumbers[0]*testNumbers[1]] + testNumbers[2:]): return True
+            elif evalSum(testValue, [testNumbers[0]+testNumbers[1]] + testNumbers[2:]): return True
+            elif evalSum(testValue, [int(str(testNumbers[0]) + str(testNumbers[1]))] + testNumbers[2:]): return True
+            else: return False
+    
+    if evalSum(testValue, testNumbers):
+        sum += testValue
+    
+print(sum)
